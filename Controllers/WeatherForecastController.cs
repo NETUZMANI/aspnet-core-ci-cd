4578b2a0d837c9a3439fce012d39be072d63bbcd
NETUZMANI <124867682+NETUZMANI@users.noreply.github.com>
2023-10-12 11:05:25 +0000

    l

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace aspnet_core_ci_cd.Controllers;
 
 [ApiController]
-[Route("/")]
+[Route("")]
 public class WeatherForecastController : ControllerBase
 {
    
